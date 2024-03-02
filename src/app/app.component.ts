1128e62b71c48b399e8687c3aee815e552b0af7d
softwareCantrips <softwarecantrips@gmail.com>
2024-03-02 17:39:35 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,40 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'soca16';
+export class AppComponent implements AfterViewInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+
+  }
+
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+    if (isPlatformBrowser(this.platformId)) {  
+      gsap.to(".gsaplogo", {
+        x: "52vw",
+        y: "15vh",
+        scale: 2.5,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".gsaplogo",
+          start: "top 80%",
+          end: "top 40%",
+          scrub: 1,
+        }
+      });
+      gsap.to(".sublogo", {
+        x: "-52vw",
+        y: "15vh",
+        scale: 2.5,
+        rotate: 360,
+        scrollTrigger: {
+          trigger: ".sublogo",
+          start: "top 80%",
+          end: "top 40%",
+          scrub: 1,
+        }
+      });
+    }
+  }
+ 
 }
